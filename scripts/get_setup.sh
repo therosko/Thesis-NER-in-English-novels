faf5e90e11508a3040ae97f13a0384329f93ed22
therosko <therosko@gmail.com>
2021-02-18 16:31:02 +0000

    add dekker et al dataset to setup steps

--- scripts/get_setup.sh
+++ scripts/get_setup.sh
@@ -13,21 +13,34 @@ mkdir /mnt/data
 echo "Cloning Litbank (raw and annotated dataset collection)"
 cd /mnt/data
 git clone https://github.com/dbamman/litbank.git #last commit https://github.com/dbamman/litbank/commit/a371cd678701fc98371355b328a1a6c4b58508a3 
-################################################
 # create output folder
 mkdir /mnt/data/gold_standard
+mkdir /mnt/data/gold_standard/litbank
 echo "Converting 4-level annotated data to 1-level gold standard"
 python3 /mnt/Git/scripts/litbank_gs_extraction.py
+
+cd /mnt/data
+# get data from Dekker et al.
+echo "Cloning Dekker et al. (raw and annotated dataset collection)"
+git clone https://github.com/Niels-Dekker/Out-with-the-Old-and-in-with-the-Novel.git
+# move relevant (raw and annotated) files and clean up
+mkdir /mnt/data/gold_standard/dekker_et_al
+mv Out-with-the-Old-and-in-with-the-Novel/NER_Experiments/New/*.gs /mnt/data/gold_standard/dekker_et_al/
+mv Out-with-the-Old-and-in-with-the-Novel/NER_Experiments/Old/*.gs /mnt/data/gold_standard/dekker_et_al/
+mkdir /mnt/data/dekker_et_al_original
+mv Out-with-the-Old-and-in-with-the-Novel/NER_Experiments/New/*.txt /mnt/data/dekker_et_al_original/
+mv Out-with-the-Old-and-in-with-the-Novel/NER_Experiments/Old/*.txt /mnt/data/dekker_et_al_original/
+rm -rf Out-with-the-Old-and-in-with-the-Novel
+echo "Converting format to fit rest"
+python3 /mnt/Git/scripts/dekkeretal_gs_extraction.py
 
 cd /mnt
 # BOOK NLP
 # clone repository
-echo "Cloning BookNLP"
-# last commit https://github.com/dbamman/book-nlp/tree/f58fbdbb018ba8bf2d836b764d0426afa0f7bc8c
+echo "Cloning BookNLP" # last commit https://github.com/dbamman/book-nlp/tree/f58fbdbb018ba8bf2d836b764d0426afa0f7bc8c
 git clone https://github.com/dbamman/book-nlp.git
-#todo uncomment in the end
-#cd book-nlp
-#git reset --hard f58fbdbb018ba8bf2d836b764d0426afa0f7bc8c
+cd book-nlp
+git reset --hard f58fbdbb018ba8bf2d836b764d0426afa0f7bc8c
 
 cd /mnt
 # install dependencies 
