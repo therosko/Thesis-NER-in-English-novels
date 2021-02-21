4409e1d8351f50482f464e93c061a92a6c9979bc
therosko <therosko@gmail.com>
2021-02-21 22:02:25 +0000

    sync rest of repo

--- docs/data.md
+++ docs/data.md
@@ -1,4 +1,12 @@
+## Litbank
+Link: TODO 
+
 Issues:
 * The entites in the novels are analysed on different levels, which means that the files do not have the same structure (num of columns).
 * The entity annotation layer of LitBank covers six of the ACE 2005 categories in text: People (PER), Facilities (FAC), Geo-political entities (GPE), Locations (LOC), Vehicles (VEH), Organizations (ORG)
 * Litbank treats words containing a hyphen as one entity (e.g. WAISTCOAT-POCKET). We split those for consistency. 
+
+## Dekker et al. 
+Link: 
+
+Only annotates people (I-PERSON) and does not differenciate between first token of entity and rest (i.e. no B-PERSON used)
---
+++ scripts/temp/d2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# navigate to book-nlp
+cd /mnt/book-nlp
+
+mkdir data/output/dekkeretal
+mkdir data/tokens/dekkeretal
+
+echo -e "\e[30;48;5;45m INFO: Running BookNLP over Dekker et al. \e[0m"
+total_count=$(ls /mnt/data/dekker_et_al/dekker_et_al_original/ppart2/ -1 | wc -l)
+current_count=1
+#iterate over original book texts and run BookNLP
+for filename in /mnt/data/dekker_et_al/dekker_et_al_original/ppart2/*.txt; do
+    bookname=$(basename -- "$filename")
+    echo -e "\e[30;48;5;45m INFO: $current_count/ $total_count: ${bookname%.*} \e[0m"
+    ./runjava novels/BookNLP -doc /mnt/data/dekker_et_al/dekker_et_al_original/ppart2/$bookname -p data/output/dekkeretal/${bookname%.*} -tok data/tokens/dekkeretal/${bookname%.*}.tokens -f
+    ((current_count++))
+done
