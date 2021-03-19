be97603c2a96afdfc5be167171dc7a1678538322
therosko <therosko@gmail.com>
2021-03-19 22:02:37 +0000

    fix: litbank extraction of NER from any column, not only first

--- scripts/litbank_gs_extraction.py
+++ scripts/litbank_gs_extraction.py
@@ -15,11 +15,29 @@ path_to_annotated_files = "/mnt/data/litbank/entities/tsv"
 gs_output_dir = "/mnt/data/gold_standard/litbank"
 
 for filename in os.listdir(path_to_annotated_files):
+    print("INFO: Current file: ", filename)
     if filename.endswith(".tsv"): 
         # read file to pandas dataframe; litbank files have 6 detenced columns (based on tabs)
         filepath = path_to_annotated_files + "/" + filename
+
         # files have different levels and therefore a different number of columns. we only take and name the first two columns
-        current_file = pd.read_csv(filepath, sep='\t', quoting=csv.QUOTE_NONE, usecols=[0,1], names=["original_word", "gs"]) 
+        current_original = pd.read_csv(filepath, sep='\t', header=None, quoting=csv.QUOTE_NONE) 
+        # create an empty dataframe
+        current_file = pd.DataFrame(columns = ['original_word', 'gs']) 
+        # move all "widest range entries" (no matter the layer)
+        for index in range(0,(len(current_original)-1)):
+            if "I-PER" in current_original.iloc[index].values:
+                new_line = {'original_word': current_original.iloc[index][0], 'gs': "I-PER"}
+                current_file = current_file.append(new_line, ignore_index=True)
+                continue
+            elif "B-PER" in current_original.iloc[index].values:
+                new_line = {'original_word': current_original.iloc[index][0], 'gs': "B-PER"}
+                current_file = current_file.append(new_line, ignore_index=True)
+                continue
+            else:
+                new_line = {'original_word': current_original.iloc[index][0], 'gs': "O"}
+                current_file = current_file.append(new_line, ignore_index=True)
+                continue
 
         # The golden standard treats hyphened compound words such as WAISTCOAT-POCKET as one word instead of three separate. We find and split those for the further analysis
         current_file = correct_hyphened(current_file)
